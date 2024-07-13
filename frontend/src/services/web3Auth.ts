0b0ef53c3d3a8fdc5f923f9aea89f8b9fb5d0610
Roman Gascoin <roman.gascoin@gmail.com>
2024-07-13 07:39:30 -0700

    Feat: circles sdk (#4)

--- frontend/src/services/web3Auth.ts
+++ frontend/src/services/web3Auth.ts
@@ -1,4 +1,4 @@
-import { CHAIN_NAMESPACES, CustomChainConfig, WEB3AUTH_NETWORK } from '@web3auth/base';
+import { CHAIN_NAMESPACES, CustomChainConfig, IProvider, WEB3AUTH_NETWORK } from '@web3auth/base';
 import { EthereumPrivateKeyProvider } from '@web3auth/ethereum-provider';
 import { MetamaskAdapter } from '@web3auth/metamask-adapter';
 import { Web3Auth } from '@web3auth/modal';
@@ -7,7 +7,9 @@ import Web3 from 'web3';
 export default class Web3AuthService {
 	private web3Auth: Web3Auth;
 
-	private web3: Web3 | null = null;
+	public web3: Web3 | null = null;
+
+	public provider?: IProvider;
 
 	constructor(
 		private clientId: string,
@@ -48,7 +50,10 @@ export default class Web3AuthService {
 
 	async login() {
 		try {
-			await this.web3Auth.connect();
+			const provider = await this.web3Auth.connect();
+			console.log('ici', provider);
+			if (provider) this.provider = provider;
+
 			return this.web3Auth.connected;
 		} catch (error) {
 			// console.error('Login error:', error);
@@ -93,102 +98,5 @@ export default class Web3AuthService {
 		}
 		const balance = await this.web3.eth.getBalance(accounts[0]);
 		return this.web3.utils.fromWei(balance, 'ether');
-	}
-
-	async signMessage(message: string): Promise<string | null> {
-		if (!this.web3) {
-			// console.error('Web3 not initialized');
-			return null;
-		}
-
-		try {
-			const accounts = await this.web3.eth.getAccounts();
-			if (accounts.length === 0) {
-				// console.error('No accounts available');
-				return null;
-			}
-			const signedMessage = await this.web3.eth.personal.sign(message, accounts[0], ''); // Assuming no password needed.
-			return signedMessage;
-		} catch (error) {
-			// console.error('Failed to sign message:', error);
-			return null;
-		}
-	}
-
-	async interactWithContract(
-		contractAddress: string,
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		abi: any,
-		methodName: string,
-		parameters: string[] = [],
-		transactionOptions = {},
-		sendTransaction = false,
-	) {
-		if (!this.web3) {
-			console.error('Web3 not initialized');
-
-			return null;
-		}
-
-		const contract = new this.web3.eth.Contract(abi, contractAddress);
-
-		const accounts = await this.web3.eth.getAccounts();
-		if (sendTransaction && accounts.length === 0) {
-			console.error('No accounts available for sending transaction');
-
-			return null;
-		}
-
-		if (sendTransaction) {
-			// Send a transaction to the contract
-			const receipt = await contract.methods[methodName](...parameters).send({
-				from: accounts[0],
-				...transactionOptions,
-			});
-			return receipt; // Returns the transaction receipt
-		}
-		// Call a view/read-only method of the contract
-		const result = await contract.methods[methodName](...parameters).call();
-		return result; // Returns the result of the read operation
-	}
-
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	async deployContract(abi: any, bytecode: string) {
-		if (!this.web3) {
-			console.error('Web3 not initialized');
-			return null;
-		}
-
-		try {
-			// Create the contract instance with the ABI
-			const contract = new this.web3.eth.Contract(abi);
-
-			// Get accounts
-			const accounts = await this.web3.eth.getAccounts();
-			if (accounts.length === 0) {
-				console.error('No accounts available');
-				return null;
-			}
-
-			console.log(bytecode);
-
-			// Deploy the contract using the provided bytecode and constructor arguments
-			const deployment = contract.deploy({
-				data: `0x${bytecode}`, // Ensure bytecode is prefixed with '0x'
-				arguments: [],
-			});
-
-			// Send the transaction from the first account with a specified gas limit and gas price
-			const deployedContract = await deployment.send({
-				from: accounts[0],
-				gas: '1500000', // Set gas limit - adjust according to your needs
-				gasPrice: '30000000000', // Set gas price - adjust according to current network conditions
-			});
-
-			return deployedContract.options.address; // Returns the deployed contract address
-		} catch (error) {
-			console.error('Failed to deploy contract:', error);
-			return null;
-		}
 	}
 }
