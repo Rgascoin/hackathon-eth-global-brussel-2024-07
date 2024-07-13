78ed6aa232b9618cb736555e653f60515611c365
cleopha <coline@seguret.net>
2024-07-13 14:21:45 +0200

    feat: add redis set en get key
    Signed-off-by: cleopha <coline@seguret.net>

--- frontend/solc.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'solc' {
-	const solc: any;
-	export default solc;
-}
-
-declare module 'solc-js' {
-	const solc: any;
-	export default solc;
-}
---
+++ frontend/src/utils/api-redis.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import axios from 'axios';
+
+interface StoreKeyResponse {
+	message?: string;
+	error?: string;
+}
+
+interface GetKeyResponse {
+	value?: any;
+	error?: string;
+}
+
+export const storeKey = async (key: string, value: any): Promise<StoreKeyResponse> => {
+	try {
+		const response = await axios.post('/api/store-key', {
+			key,
+			value,
+		});
+		return response.data;
+	} catch (error) {
+		console.error('Error storing the key:', error);
+		return { error: 'Error storing the key' };
+	}
+};
+
+export const getKey = async (key: string): Promise<GetKeyResponse> => {
+	try {
+		const response = await axios.get('/api/store-key', {
+			params: { key },
+		});
+		return response.data;
+	} catch (error) {
+		console.error('Error retrieving the key:', error);
+		return { error: 'Error retrieving the key' };
+	}
+};
